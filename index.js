a8d858020104f16ac4ed2d5640b2f9431212f296
Courtney Macgregor <cjoss90@hotmail.com>
2024-07-18 18:57:18 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solutions in this file
+const message = [];
+
+function writeCards(message) {
+    let writeCards = ["Guadalupe", "Ollie", "Aki"];
+    for (let i = 0; i < writeCards.length; i++) {
+    message.splice(i, 2, `Thank you, ${writeCards[i]}, for the wonderful surprise gift!`);
+    }
+
+    return(message);
+}
+
+writeCards(message);
+
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
